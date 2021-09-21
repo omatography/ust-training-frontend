8d0b39a0192add91dce802a29392d161619d44a4
Omkar R <40696201+omatography@users.noreply.github.com>
2021-09-21 12:01:05 +0530

    table

---
+++ frontend/my-first-app/src/app/Course.ts
@@ -0,0 +1,6 @@
+export class Course{
+
+    constructor(public id:number, public title:string, public summary:string, public img:string  ){
+        
+    }
+}
--- frontend/my-first-app/src/app/card.component.ts
+++ frontend/my-first-app/src/app/card.component.ts
@@ -3,11 +3,11 @@ import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: "card-comp",
-    template: `<div class="card" style="width: 18rem;">
-    <img src={{img}} class="card-img-top" alt="..." height="400">
+    template: `<div class="card" style="width: 300px;">
+    <img src={{img}} class="card-img-top" alt="..." height="200"  >
     <div class="card-body">
       <h5 class="card-title">{{title}}</h5>
-      <p class="card-text">{{desc}}</p>
+      <p class="card-text"><ng-content></ng-content></p>
       <a href={{img}} class="btn btn-primary">Know more?</a>
     </div>
   </div>`,
@@ -17,6 +17,5 @@ import { Title } from '@angular/platform-browser';
 export class CardComponent extends Component{
 
     @Input('title') title:string ="Null";
-    @Input('desc') desc:string ="Null";
-    @Input('img') img:string ="https://images.unsplash.com/photo-1545161296-d9c2c241f2ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" ;
+    @Input('img') img:string ="Null";
 }
